40763d540a5ac90815b455a0345bb029e94a7313
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-03-04 16:11:57 +0700

    feat: add CORS origin for frontend

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Invoice, InvoiceItem
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Invoice)
+admin.site.register(InvoiceItem)
