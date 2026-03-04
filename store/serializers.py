480b7c969dd7e8a80405d5e572731b616c582872
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-03-04 16:20:31 +0700

    refactor: refactor fields returned when getting invoice items

--- store/serializers.py
+++ store/serializers.py
@@ -8,11 +8,16 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class InvoiceItemSerializer(serializers.ModelSerializer):
-    product_id = serializers.IntegerField(write_only=True)
+    product_name = serializers.CharField(source="product.name", read_only=True)
+    price = serializers.DecimalField(source="product.price", max_digits=10, decimal_places=2, read_only=True)
+    subtotal = serializers.SerializerMethodField()
 
     class Meta:
         model = InvoiceItem
-        fields = ['product_id', 'quantity']
+        fields = ["product_name", "quantity", "price", "subtotal"]
+
+    def get_subtotal(self, obj):
+        return obj.quantity * obj.product.price
 
 class InvoiceSerializer(serializers.ModelSerializer):
     items = InvoiceItemSerializer(many=True)
