a86f346890838cb3983414faaebb3ba9a5df5732
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2026-03-04 14:33:04 +0700

    feat: add product, invoice, and invoice item models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import date
+import uuid
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+class Invoice(models.Model):
+    invoice_number = models.CharField(max_length=20, unique=True)
+    date = models.DateField(default=date.today)
+    customer_name = models.CharField(max_length=100)
+    total = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.invoice_number
+    
+class InvoiceItem(models.Model):
+    invoice = models.ForeignKey(
+        Invoice,
+        related_name="items",
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    subtotal = models.IntegerField()
